45c3859cb419946950fe5ec6157026815d1c0467
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2016-06-24 15:13:53 +0200

    No error message when Mysql is not yet started

--- service/mysql.sh
+++ service/mysql.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+for i in $(seq 1 90); do
+    docker exec -i $1 bash -c 'mysql -p$MYSQL_ROOT_PASSWORD -e "SHOW DATABASES"' > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        break
+    fi
+done
 docker exec -i $1 bash -c 'mysql -p$MYSQL_ROOT_PASSWORD -e "SET GLOBAL max_allowed_packet = 104857600;"'
