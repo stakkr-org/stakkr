2b7016c9877ed14985c76b0ff0a96e9105efe154
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2016-06-28 15:36:29 +0200

    added setuptools

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-PIP_PATH=$(/bin/which pip3)
-if [ -z "$PIP_PATH" ]; then
-    sudo apt-get -y install python3-pip python3-setuptools
-fi
+sudo apt-get -y install python3-pip python3-setuptools
 
 pip3 install -e .
