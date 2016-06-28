f73b13125d3c8d4ed7bf419c28c12558dc559ad0
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2016-06-28 12:39:59 +0200

    Added an install script and use clint to colorize. Status displays now IP, and removed many useless functions, replaced by a single one to get a VM item such as its name or ip

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from setuptools import setup, find_packages
+
+setup(name='docker-lamp',
+      version='0.1',
+      description='A stack based on docker to run PHP Applications',
+      url='http://github.com/inetprocess/lamp',
+      author='Emmanuel Dyan',
+      author_email='emmanuel.dyan@inetprocess.com',
+      license='Apache 2.0',
+      packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+      install_requires=[
+          'clint',
+      ])
