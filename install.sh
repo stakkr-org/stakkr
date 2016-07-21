c446a350e774f8511bd22bf7743e79f1ee41db85
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2016-07-21 16:21:58 +0200

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-sudo apt-get -y install python3-pip python3-setuptools python3-virtualenv
-virtualenv -p /usr/bin/python3 ${PWD##*/}_lamp
-source ${PWD##*/}_lamp/bin/activate
-pip3 install click
-pip3 install -e .
-deactivate
+echo "Installing various python packages"
+sudo apt-get -y install python3-pip python3-setuptools python3-virtualenv > /dev/null
+
+echo "Creating the virtualenv"
+virtualenv -p /usr/bin/python3 ${PWD##*/}_lamp > /dev/null
+source ${PWD##*/}_lamp/bin/activate > /dev/null
+pip3 install click > /dev/null
+pip3 install -e . > /dev/null
+
+echo "Leaving the virtualenv. To enter it again do : "
+echo "source \${PWD##*/}_lamp/bin/activate"
+deactivate > /dev/null
