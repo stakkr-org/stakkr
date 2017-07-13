b9a1a47361df09c4ef9b717afd6358aff982ecc5
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2017-07-13 11:20:34 +0200

    Prepare to use marina as a package

--- setup.py
+++ setup.py
@@ -1,6 +1,19 @@
 from marina.plugins import get_plugins_configuration
 from setuptools import setup, find_packages
 
+def get_console_scripts():
+    """Guess if we use marina as a package or if it has been cloned"""
+
+    scripts = "[console_scripts]\n"
+    try:
+        from marina import cli, docker_clean
+        scripts += "marina=marina.cli:main\n"
+        scripts += "docker-clean=marina.docker_clean:main\n"
+    except Exception:
+        scripts += "marina=cli:main\n"
+        scripts += "docker-clean=docker_clean:main\n"
+
+    return scripts
 
 setup(
     name='Marina',
@@ -12,17 +25,12 @@ setup(
     license='Apache 2.0',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     py_modules=['marina'],
-    entry_points='''
-        [console_scripts]
-        marina=cli:main
-        docker-clean=docker_clean:main
-        {}
-      '''.format(get_plugins_configuration()),
+    entry_points='{}{}'.format(get_console_scripts(), get_plugins_configuration()),
     install_requires=[
         'clint',
         'click', 'click-plugins',
-        'requests>=2.11.0,<2.12',
         'docker-compose',
-        'configobj'
+        'configobj',
+        'requests>=2.11.0,<2.12'
         ]
 )
