26240a1a3b79467fd95f072d8db8f25602bc8e54
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2017-07-31 17:27:20 +0200

    Bumped to v3.2.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ sys.path.insert(0, here)
 
 setup(
     name='stakkr',
-    version='3.2',
+    version='3.2.1',
     description='A configurable stack based on docker to run any combination of services (PHP, MySQL, Apache, Nginx, Mongo, etc..)',
     long_description=""".. image:: https://raw.githubusercontent.com/edyan/stakkr/master/docs/stakkr-logo.png
     :width: 200px
