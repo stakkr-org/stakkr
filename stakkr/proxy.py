be6cff09f7551ce540eb3f9220fd991ce0732c36
Emmanuel Dyan <emmanueldyan@gmail.com>
2018-07-29 10:59:44 +0200

    Proxy class init

--- stakkr/proxy.py
+++ stakkr/proxy.py
@@ -10,21 +10,26 @@ from stakkr.configreader import Config
 class Proxy():
     """Main class that does actions asked in the cli"""
 
-    proxy_name = 'stakkr_proxy'
+    def __init__(self, port: int = 80, proxy_name: str = 'stakkr_proxy'):
+        """Set the right values to start the proxy"""
 
+        self.port = port
+        self.proxy_name = proxy_name
 
-    def __init__(self, port: int):
-        pass
+    def start(self, stakkr_network: str):
+        """Start stakkr proxy if stopped"""
 
-
-    def start(self, connect_network: str):
-        if container_running[self.proxy_name] is False:
+        if container_running[self.proxy_name] is True:
             return
 
         api_client = docker_actions.get_api_client()
+        # Start the CT
 
-        docker_actions.add_container_to_network(self.proxy_name, connect_network)
-
+        # Connect it to the main network
+        docker_actions.add_container_to_network(self.proxy_name, stakkr_network)
 
     def stop(self):
-        pass
+        """Stop stakkr proxy"""
+
+        if container_running[self.proxy_name] is False:
+            return
