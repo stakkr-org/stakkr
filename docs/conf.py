a0408f11803dd061fe260bbe5fa57d4e683843ac
Emmanuel Dyan <emmanueldyan@gmail.com>
2019-01-27 20:06:37 +0100

    "Bump version 4.0rc1"

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ author = 'Emmanuel Dyan'
 # built documents.
 #
 # The short X.Y version.
-version = '4.0b5'
+version = '4.0rc1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
