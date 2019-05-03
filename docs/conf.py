ead3ad5a5f0d50b4dd8f1b797bf8ef8261b141db
Emmanuel Dyan <emmanueldyan@gmail.com>
2019-05-03 20:04:17 +0200

    "Bump version 4.0"

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ author = 'Emmanuel Dyan'
 # built documents.
 #
 # The short X.Y version.
-version = '4.0rc3'
+version = '4.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ extra_packages = []
 if os.name == 'nt':
     extra_packages.append('pypiwin32')
 
-__version__ = '4.0rc3'
+__version__ = '4.0'
 
 # Get the long description from the README file
 def readme():
