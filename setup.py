b2f1038a4d4ec8ac26550426f65bcd273e3ebf1f
Emmanuel Dyan <emmanueldyan@gmail.com>
2019-07-06 22:41:57 +0200

    "Bump version 4.1.2"

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ extra_packages = []
 if os.name == 'nt':
     extra_packages.append('pypiwin32')
 
-__version__ = '4.1.0'
+__version__ = '4.1.2'
 
 # Get the long description from the README file
 def readme():
